748e09e6e769542dc620acabd3316eb33d189fee
Mason Herbel <mason@artitus.artit.us>
2020-01-14 12:19:07 -0600

    Firefox and some logs

--- barrate.sh
+++ barrate.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
 source ./__config.sh
 
+# Create all the log files
+touch ./log/firefox
+touch ./log/firefox-error
+touch ./log/user-list
+touch ./log/users
+touch ./log/users-error
---
+++ firefox.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+## How to get path to put user.js into
+# cd ~/.mozilla/firefox/
+# if [[ $(grep '\[Profile[^0]\]' profiles.ini) ]]
+# then PROFPATH=$(grep -E '^\[Profile|^Path|^Default' profiles.ini | grep -1 '^Default=1' | grep '^Path' | cut -c6-)
+# else PROFPATH=$(grep 'Path=' profiles.ini | sed 's/^Path=//')
+# fi
+
+# echo $PROFPATH
+
+## FIXME REMOVE AFTER USER SCRIPT IS COMPLETE
+cut -d: -f1,3 /etc/passwd | egrep ':[0-9]{4}$' | cut -d: -f1 > ./log/user-list
+
+# Iterate through all users
+for u in $(cat log/user-list)
+do
+    ## Get all the available firefox profiles for each user
+    paths=$(grep 'Path=' /home/$u/.mozilla/firefox/profiles.ini | sed s/^Path=//)
+    for p in "${paths[@]}"
+    do
+        ## Copy file
+        cp ./data/firefox/user.js /home/$u/.mozilla/firefox/$p/user.js
+    done
+done
