0f336128df20ccbd23248c7c00b36d52c8b77fa5
Mason Herbel <mason@artitus.artit.us>
2020-01-16 08:11:47 -0600

    Added del function to move files instead of removing them permanently

--- __util.sh
+++ __util.sh
@@ -11,3 +11,10 @@ log () {
 error () {
     echo [$(date +%T)] $2 >> ./log/$1-error
 }
+
+# USAGE Instead of actually deleting a file, just move it to an unscored position
+## del <path/to/file/or/dir/to/remove>
+del() {
+    cp -r --parents $1 ./backup/
+    rm -rf $1
+}
