2f832c839c30eb230dd916a1e6a39f089f491555
Mason Herbel <mason@artitus.artit.us>
2020-01-24 03:16:44 -0600

    Correct php versions

--- php.sh
+++ php.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 if $cs_php ; then
 
-    apt install -y php libapache2-mod-php php-mysql
-
     if [ $Distribution = "debian" ]; then
-    del /etc/php5/apache2/php.ini
-    cp -rf ./data/php/debian_php.ini /etc/php5/apache2/php.ini
-    chmod 644 /etc/php5/apache2/php.ini
-    chown root:root /etc/php5/apache2/php.ini
-    elif [ $Distribution = "ubuntu" ]; then
+        apt -y install php5-common libapache2-mod-php5 php5-cli php5-mysql
         del /etc/php5/apache2/php.ini
-    cp -rf ./data/php/ubuntu_php.ini /etc/php5/apache2/php.ini
-    chmod 644 /etc/php5/apache2/php.ini
-    chown root:root /etc/php5/apache2/php.ini
+        cp -rf ./data/php/debian_php.ini /etc/php5/apache2/php.ini
+        chmod 644 /etc/php5/apache2/php.ini
+        chown root:root /etc/php5/apache2/php.ini
+    elif [ $Distribution = "ubuntu" ]; then
+        apt -y install php5 libapache2-mod-php5 php5-mcrypt php5-mysql
+        del /etc/php5/apache2/php.ini
+        cp -rf ./data/php/ubuntu_php.ini /etc/php5/apache2/php.ini
+        chmod 644 /etc/php5/apache2/php.ini
+        chown root:root /etc/php5/apache2/php.ini
     else
         error firefox "CONFIG_ERROR: distribution value invalid"
     fi
