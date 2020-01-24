f91c443605964fbe0e7fa25a4e532708c4842ba0
Mason Herbel <mason@artitus.artit.us>
2020-01-23 21:58:35 -0600

    Final Passwords Module

--- passwords.sh
+++ passwords.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+apt install -y libpam-modules
+
+del /etc/pam.d/common-auth
+del /etc/pam.d/common-password
+
+cp -f ./data/passwords/common-auth /etc/pam.d/common-auth
+cp -f ./data/passwords/common-password /etc/pam.d/common-password
+
+del /etc/login.defs
+cp -f ./data/passwords/login.defs /etc/login.defs
